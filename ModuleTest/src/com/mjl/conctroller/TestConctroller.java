2ff6728491e82b2ad3971a05bfd3964a67cd3ef7
tomcat_M <majianlongwow@163.com>
2020-06-18 13:11:52 +0800

    第一次提交本地仓库

---
+++ ModuleTest/src/com/mjl/conctroller/TestConctroller.java
@@ -0,0 +1,19 @@
+package com.mjl.conctroller;
+
+/**
+ * @Auther: mjl
+ * @Date: 2020/6/18 - 13:04
+ * @Description: com.mjl.conctroller
+ * @version: 1.0
+ */
+public class TestConctroller {
+    public String getMember(){
+        System.out.println("--==================");
+        return  "success";
+    }
+    public static void main(String[] args) {
+        TestConctroller testConctroller = new  TestConctroller();
+        System.out.println("00000000000000000000000000000000");
+        testConctroller.getMember();
+    }
+}
